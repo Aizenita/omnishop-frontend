89170ab31781d47ecddcd8149fd3af50771a6ce6
Aizenita <dvillamor20731@iesjoanramis.org>
2025-05-28 22:11:14 +0200

    added quick cart button

--- src/app/features/landing/landing-page/landing-page.component.ts
+++ src/app/features/landing/landing-page/landing-page.component.ts
@@ -9,6 +9,8 @@ import { AuthService, UserIdentity } from '../../../shared/services/auth.service
 import { ButtonModule } from 'primeng/button';
 import { CartService } from '../../../shared/services/cart.service'; // Nueva importación
 import { BadgeModule } from 'primeng/badge'; // Nueva importación
+import { ToastModule } from 'primeng/toast'; // NUEVA importación
+import { MessageService } from 'primeng/api'
 
 @Component({
   selector: 'app-landing-page',
@@ -17,8 +19,10 @@ import { BadgeModule } from 'primeng/badge'; // Nueva importación
     CommonModule, 
     RouterLink, 
     ButtonModule,
-    BadgeModule  // Añadir BadgeModule
+    BadgeModule,  // Añadir BadgeModule
+    ToastModule  // AÑADIR ToastModule
   ],
+  providers: [MessageService], // AÑADIR MessageService a providers
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
@@ -36,7 +40,9 @@ export class LandingPageComponent implements OnInit {
     private categoryService: CategoryService,
     private authService: AuthService,
     private cartService: CartService, // Inyectar CartService
-    private router: Router
+    private router: Router,
+    private messageService: MessageService // INYECTAR MessageService
+
   ) {
     this.isAuthenticated$ = this.authService.isAuthenticated$;
     this.currentUser$ = this.authService.currentUser$;
@@ -51,6 +57,25 @@ export class LandingPageComponent implements OnInit {
     this.categoryService.getCategories().subscribe(data => {
       this.categories = data;
     });
+  }
+   quickAddToCart(product: Product): void {
+    if (product && product.id) {
+      this.cartService.addItem(product, 1); // Añade 1 unidad por defecto
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Producto Añadido',
+        detail: `${product.nombre} ha sido añadido a tu carrito.`,
+        life: 3000 // Duración del toast en milisegundos
+      });
+    } else {
+      console.error('Intento de añadir producto inválido o sin ID:', product);
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'No se pudo añadir el producto al carrito.',
+        life: 3000
+      });
+    }
   }
 
   logout(): void {
